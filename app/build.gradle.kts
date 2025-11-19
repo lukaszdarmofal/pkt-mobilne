a9a83c86c2a8b57562eae61dbc7a0236c27bee4b
lukasz <lukasz.darmofal@cyfroweszkoly.pl>
2025-11-19 12:26:36 +0100

    Corrected build.gradle.kts file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
