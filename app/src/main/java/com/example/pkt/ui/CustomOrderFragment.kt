7e28c5506bc74ba83773689e69748c898508cb0d
lukasz <lukasz.darmofal@cyfroweszkoly.pl>
2025-12-01 13:23:35 +0100

    Create custom order layout

--- app/src/main/java/com/example/pkt/ui/CustomOrderFragment.kt
+++ app/src/main/java/com/example/pkt/ui/CustomOrderFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.pkt.R
+import com.example.pkt.databinding.FragmentCustomOrderBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +24,15 @@ class CustomOrderFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentCustomOrderBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,8 +45,17 @@ class CustomOrderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_order, container, false)
+        _binding = FragmentCustomOrderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submitCustomOrder.setOnClickListener {
+            findNavController().navigate(R.id.action_customOrderFragment_to_summaryFragment)
+        }
+
     }
 
     companion object {
